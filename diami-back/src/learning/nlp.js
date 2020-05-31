76846c0cfcafcd3682a0f4a0b089f821e84dcf18
David Diaz <ing.daviid@gmail.com>
2020-05-31 05:52:46 -0500

    Chat en home del primer factor y emit notification

--- diami-back/src/learning/nlp.js
+++ diami-back/src/learning/nlp.js
@@ -24,26 +24,29 @@ var newUserTemp = {
   registroFlag = false,
   sesionFlag = false;
 
+var factoresUsuario = {
+  autoestima: { entornoFamiliar: 0, espejo: 0, satisfacion: 0 },
+  culpa: { meHablan: 0, exitos: 0, errores: 0 },
+  social: { frecuencia: 0, salir: 0, reuniones: 0 }
+};
+
 class Nlp {
   constructor() {}
 
   // Funciones de NLP
 
   initChatNlpLogin(client) {
-
-
     newUserTemp = {
-        session_id: '',
-        name: '',
-        email: '',
-        password: '',
-        img: '',
-        role: '',
-        messageTemp: ''
-      };
-      registroFlag = false;
-      sesionFlag = false;
-
+      session_id: '',
+      name: '',
+      email: '',
+      password: '',
+      img: '',
+      role: '',
+      messageTemp: ''
+    };
+    registroFlag = false;
+    sesionFlag = false;
 
     assistant
       .createSession({
@@ -80,151 +83,272 @@ class Nlp {
   }
 
   initChatNlpLoginMessage(client, msj) {
-
-
     if (newUserTemp['email'] != '' && sesionFlag == true) {
+      client.emit('message-login', {
+        user: '',
+        type: 'BOT',
+        message: 'Comprobando la sesión....',
+        createdAt: new Date()
+      });
 
-        
+      setTimeout(() => {
+        client.emit('login-success', {
+          email: newUserTemp['email'],
+          password: msj,
+          isLogin: true
+        });
+      }, 1000);
+
+      setTimeout(() => {
+        newUserTemp = {
+          session_id: '',
+          name: '',
+          email: '',
+          password: '',
+          img: '',
+          role: '',
+          messageTemp: ''
+        };
+        registroFlag = false;
+        sesionFlag = false;
+      }, 2000);
+    } else {
+      if (newUserTemp['email'] != '' && newUserTemp['name'] != '') {
+        newUserTemp['password'] = msj;
         client.emit('message-login', {
-            user: '',
-            type: 'BOT',
-            message: "Comprobando la sesión....",
-            createdAt: new Date()
+          user: '',
+          type: 'BOT',
+          message: newUserTemp['messageTemp'],
+          createdAt: new Date()
+        });
+
+        console.log(newUserTemp);
+        setTimeout(() => {
+          client.emit('login-success', {
+            name: newUserTemp['name'],
+            email: newUserTemp['email'],
+            password: msj,
+            isLogin: false
           });
-    
-          setTimeout(() => {
-            client.emit('login-success', {              
-              email: newUserTemp['email'],
-              password: msj,
-              isLogin: true
-            });
-          }, 1000);
+        }, 2000);
 
-          setTimeout(() => {
+        setTimeout(() => {
+          newUserTemp = {
+            session_id: '',
+            name: '',
+            email: '',
+            password: '',
+            img: '',
+            role: '',
+            messageTemp: ''
+          };
+          registroFlag = false;
+          sesionFlag = false;
+        }, 3000);
+      } else {
+        assistant
+          .message({
+            assistantId: assistantId,
+            sessionId: newUserTemp['session_id'],
+            input: {
+              message_type: 'text',
+              text: msj
+            }
+          })
+          .then((res) => {
+            //console.log(JSON.stringify(res.result, null, 2));
+            if (res.result.output.entities.length >= 1) {
+              for (let i = 0; i < res.result.output.entities.length; i++) {
+                const entityDetected = res.result.output.entities[i];
 
-            newUserTemp = {
-                session_id: '',
-                name: '',
-                email: '',
-                password: '',
-                img: '',
-                role: '',
-                messageTemp: ''
-              };
-              registroFlag = false;
-              sesionFlag = false;
+                switch (entityDetected.entity) {
+                  case 'nombre':
+                    newUserTemp['name'] = entityDetected.value;
+                    break;
 
-            
-          }, 2000);
+                  case 'correo':
+                    newUserTemp['email'] = msj.substring(
+                      entityDetected.location[0],
+                      entityDetected.location[1]
+                    );
+                    break;
 
+                  default:
+                    console.log('Entidad desconocida');
+                    break;
+                }
+              }
+            }
 
-    }else{
-        if (newUserTemp['email'] != '' && newUserTemp['name'] != '') {
-      
-            newUserTemp['password']=msj
+            if (
+              res.result.output.intents[0] != undefined &&
+              res.result.output.intents[0].intent == 'iniciarSesion'
+            )
+              sesionFlag = true;
+            else {
+              if (
+                res.result.output.intents[0] != undefined &&
+                res.result.output.intents[0].intent == 'registroAPP'
+              )
+                registroFlag = true;
+            }
+
+            if (newUserTemp['email'] != '' && newUserTemp['name'] != '') {
+              newUserTemp['messageTemp'] = res.result.output.generic[0].text;
+              res.result.output.generic[0].text =
+                'Pude anotar casi todo, finalmente cuál será tu contraseña?. Debe ser mayor a 6 caracteres. Digíta solo la contraseña porfavor';
+            }
+
             client.emit('message-login', {
               user: '',
               type: 'BOT',
-              message: newUserTemp['messageTemp'],
+              message: res.result.output.generic[0].text,
               createdAt: new Date()
             });
-      
-            console.log(newUserTemp);
-            setTimeout(() => {
-              client.emit('login-success', {
-                name: newUserTemp['name'],
-                email: newUserTemp['email'],
-                password: msj,
-                isLogin: false
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    }
+  }
+
+  initChat(client, msj,user) {
+    createSessionInitChat()
+      .then((res) => {
+        assistant
+          .message({
+            assistantId: assistantId,
+            sessionId: newUserTemp['session_id'],
+            input: {
+              message_type: 'text',
+              text: msj
+            }
+          })
+          .then((res) => {
+            //console.log(JSON.stringify(res.result, null, 2));
+
+            for (let i = 0; i < res.result.output.generic.length; i++) {
+              const genericText = res.result.output.generic[i].text;
+              client.emit('message', {
+                user: '',
+                type: 'BOT',
+                message: res.result.output.generic[i].text,
+                createdAt: new Date()
               });
-            }, 2000);
+            }
 
-            setTimeout(() => {
-
-                newUserTemp = {
-                    session_id: '',
-                    name: '',
-                    email: '',
-                    password: '',
-                    img: '',
-                    role: '',
-                    messageTemp: ''
-                  };
-                  registroFlag = false;
-                  sesionFlag = false;
-    
-                
-              }, 3000);
-
-
-
-          } else {
-            assistant
-              .message({
-                assistantId: assistantId,
-                sessionId: newUserTemp['session_id'],
-                input: {
-                  message_type: 'text',
-                  text: msj
+            if (res.result.output.generic.length == 2) {
+              if (res.result.output.generic[0].text.includes('familia')) {
+                if (
+                  res.result.output.entities[0].entity == 'no' ||
+                  res.result.output.entities[0].entity == 'poco'
+                ) {
+                  factoresUsuario['autoestima']['entornoFamiliar'] = 3;
                 }
+              }
+
+              if (res.result.output.generic[0].text.includes('espejo')) {
+                if (
+                  res.result.output.entities[0].entity == 'no' ||
+                  res.result.output.entities[0].entity == 'poco'
+                ) {
+                  factoresUsuario['autoestima']['espejo'] = 3;
+                }
+              }
+
+              if (
+                res.result.output.generic[0].text.includes('taller') ||
+                res.result.output.generic[0].text.includes('satisfecho')
+              ) {
+                if (
+                  res.result.output.entities[0].entity == 'no' ||
+                  res.result.output.entities[0].entity == 'poco'
+                ) {
+                  factoresUsuario['autoestima']['satisfacion'] = 3;
+                }
+              }
+            }
+
+            var total=factoresUsuario['autoestima']['entornoFamiliar'] +
+            factoresUsuario['autoestima']['espejo'] +
+            factoresUsuario['autoestima']['satisfacion'];
+            if (total>=6) {
+
+              var situation="Riesgo bajo";
+              if (total>=11 && total <=18) {
+
+                situation="Riesgo medio"
+                
+              }
+              else{
+
+                  if (total>=19 ) {
+
+                    situation="Riesgo Alto"
+                    
+                  }
+
+              }
+
+              client.emit('notification', {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                img: '',
+                phone:'',
+                edad:'',
+                situation:situation
+              });
+
+            }
+          })
+          .catch((err) => {
+            assistant
+              .createSession({
+                assistantId: assistantId
               })
               .then((res) => {
-                //console.log(JSON.stringify(res.result, null, 2));
-                if (res.result.output.entities.length >= 1) {
-                  for (let i = 0; i < res.result.output.entities.length; i++) {
-                    const entityDetected = res.result.output.entities[i];
-      
-                    switch (entityDetected.entity) {
-                      case 'nombre':
-                        newUserTemp['name'] = entityDetected.value;
-                        break;
-      
-                      case 'correo':
-                        newUserTemp['email'] = msj.substring(
-                          entityDetected.location[0],
-                          entityDetected.location[1]
-                        );
-                        break;
-      
-                      default:
-                        console.log('Entidad desconocida');
-                        break;
-                    }
-                  }
-                }
-      
-                if (res.result.output.intents[0]!=undefined && res.result.output.intents[0].intent == 'iniciarSesion')
-                  sesionFlag = true;
-                else {
-                  if (res.result.output.intents[0]!=undefined && res.result.output.intents[0].intent == 'registroAPP')
-                    registroFlag = true;
-                }
-      
-                if (newUserTemp['email'] != '' && newUserTemp['name'] != '') {
-                  newUserTemp['messageTemp'] = res.result.output.generic[0].text;
-                  res.result.output.generic[0].text =
-                    'Pude anotar casi todo, finalmente cuál será tu contraseña?. Debe ser mayor a 6 caracteres. Digíta solo la contraseña porfavor';
-                }
-      
-                client.emit('message-login', {
+                newUserTemp['session_id'] = res.result.session_id;
+                client.emit('message', {
                   user: '',
                   type: 'BOT',
-                  message: res.result.output.generic[0].text,
+                  message: 'Perdón, me distraje un poco, podrías repetirme?',
                   createdAt: new Date()
                 });
               })
               .catch((err) => {
                 console.log(err);
               });
-          }
-
-    }
-
-
-   
-
-
+          });
+      })
+      .catch((err) => {
+        console.log('error creando sessión');
+      });
   }
+
+  getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+}
+
+function createSessionInitChat() {
+  return new Promise((resolve, reject) => {
+    if (newUserTemp['session_id'] == '') {
+      assistant
+        .createSession({
+          assistantId: assistantId
+        })
+        .then((res) => {
+          newUserTemp['session_id'] = res.result.session_id;
+          resolve([]);
+        })
+        .catch((err) => {
+          console.log(err);
+          reject([]);
+        });
+    } else resolve([]);
+  });
 }
 
 module.exports = Nlp;
--- diami-back/src/sockets/socket.js
+++ diami-back/src/sockets/socket.js
@@ -1,37 +1,38 @@
 const { io } = require('../index');
 const nlp = require('../learning/nlp');
-
+const saludosInitChat = [
+  'Hola de nuevo',
+  'Siempre me gusta empezar con un gran Hola!!',
+  'Estaba tomando una siesta, Hola!'
+];
 const _nlp = new nlp();
 
 io.on('connection', (client) => {
+  client.on('init-chat-login', (userId, callback) => {
+    _nlp.initChatNlpLogin(client);
+  });
 
-    client.on('init-chat-login', (userId, callback) => {        
-        _nlp.initChatNlpLogin(client);        
+  client.on('init-chat', (userId, callback) => {
+    console.log(userId);
+
+    // _nlp.initChatNlpLogin(client);
+
+    client.emit('message', {
+      user: client.id,
+      type: 'BOT',
+      message: saludosInitChat[_nlp.getRandomArbitrary(0,saludosInitChat.length)],
+      createdAt: new Date()
     });
+  });
 
-    client.on('init-chat', (userId, callback) => {
-        console.log(userId);
+  client.on('disconnect', () => {
+    console.log('Usuario desconectado');
+  });
 
-       // _nlp.initChatNlpLogin(client);    
+  client.on('send-message-login', (msj, callback) => {
+    _nlp.initChatNlpLoginMessage(client, msj.text.trim());
 
-        /*client.emit('message', {
-            user: client.id,
-            type: 'BOT',
-            message: 'Hola de nuevo',
-            createdAt: new Date()
-        });*/
-    });
-
-    client.on('disconnect', () => {
-        console.log('Usuario desconectado');
-    });
-
-    client.on('send-message-login', (msj, callback) => {
-
-
-        _nlp.initChatNlpLoginMessage(client,msj.text.trim());   
-
-        /*setTimeout(() => {
+    /*setTimeout(() => {
             client.emit('message-login', {
                 user: client.id,
                 type: 'BOT',
@@ -39,16 +40,11 @@ io.on('connection', (client) => {
                 createdAt: new Date()
             });
         }, 2000);*/
-    });
+  });
 
-    client.on('send-message', (msj, callback) => {
-        setTimeout(() => {
-            client.emit('message', {
-                user: client.id,
-                type: 'BOT',
-                message: msj.text,
-                createdAt: new Date()
-            });
-        }, 2000);
-    });
+  client.on('send-message', (msj, callback) => {
+    _nlp.initChat(client, msj.text.trim(),msj.user);
+
+    
+  });
 });
